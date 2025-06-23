3c4483fa5274cc32b211502d343431e5ff391cd9
eragondr <thagsau@gmail.com>
2025-06-23 21:28:11 +0700

    student info

--- Student/contract/Caculate.sol
+++ Student/contract/Caculate.sol
@@ -1,2 +1,46 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
-import StudentInfo;
+import "./StudentInfo.sol";
+
+contract Caculte{
+
+    StudentInfo[] private students;
+    function CreateNewStudent(int256 id, uint32[3] calldata grades,address wallet, StudentType studentType) public 
+    {
+        StudentInfo student = new StudentInfo();
+        student.SetStudentId(id);
+        student.SetStudentGrade(id, grades);
+        student.SetStudentWallet(id,wallet);
+        student.SetStudentType(studentType);
+        students.push(student);
+    }
+
+    function CaculateGrade(StudentInfo student) private view returns (uint32 ) {
+        uint32 average = 0;  // initialize average to zero
+        Grade memory grade = student.GetStudentGrade(student.GetStudentId());
+        average += grade.mark1;
+        average += grade.mark2;
+        average += grade.mark3;
+        average = average/3;  
+        return average;
+     
+    }
+    
+    function ViewStudent( uint index)public view returns (StudentInfo ) {  // This is the
+        StudentInfo student = StudentInfo(address (students[index]));
+        return student;
+    }
+    function ViewStudents()public view returns (StudentInfo[]memory) {  // This is the
+        return students;
+    }
+    function ViewStudentWallet(uint16 index)public view returns (address ) {  // This is the
+         StudentInfo student = StudentInfo(address (students[index]));
+        return student.GetStudentWallet(student.GetStudentId());
+    }
+    function ViewStudentWallet2(uint16 index)public view returns (address ) {  // This is the
+         StudentInfo student = students[index];
+        return student.GetStudentWallet(student.GetStudentId());
+    }
+
+//213,[10,9,8],0x1Ae4C2EcD3061D966dF87d800C59250196d6cabd,POOR
+}
--- Student/contract/StudentInfo.sol
+++ Student/contract/StudentInfo.sol
@@ -17,20 +17,19 @@ enum StudentType
 
 contract StudentInfo {
     
-    int256 public  id;
-    string public name;
-    StudentType public student_type;
+    int256 private  id;
+    StudentType private student_type;
     // Mapping to store studentId and  wallet addresses
-    mapping(int256 => address) public student_wallet;
+    mapping(int256 => address)  private student_wallet;
     // Contract balance (e.g., for funding purposes)
-    uint256 public balance;
+    uint256 private balance;
     // Mapping to store grades for each student by ID
-    mapping(int256 => Grade) public student_grades;
+    mapping(int256 => Grade) private student_grades;
     // Event to log grade updates
     event GradeUpdated(int256 studentId, uint32 mark1, uint32 mark2, uint32 mark3);
     
-    // Function to set grades for a student
-    function setGrade(int256  _studentId, uint32[3] memory _grades) public {
+    // Function to Set grades for a student
+    function SetStudentGrade(int256  _studentId, uint32[3] calldata _grades) public {
         // Validate grades (e.g., ensure marks are within a valid range)
         require(_grades[0] <= 100 && _grades[1] <= 100 && _grades[2] <= 100, "Marks must be <= 100");
         
@@ -45,20 +44,42 @@ contract StudentInfo {
         emit GradeUpdated(_studentId, _grades[0], _grades[1], _grades[2]);
     }
 
-    // Function to get grades for a student
-    function getGrade(int256  _studentId) public view returns (Grade memory) {
+
+    // Function to Get grades for a student
+    function GetStudentGrade(int256  _studentId) public view returns (Grade memory) {
         return student_grades[_studentId];
     }
 
-    // Function to set student wallet address
-    function setStudentWallet(int256  _studentId, address _wallet) public {
+    // Function to Set student wallet address
+    function SetStudentWallet(int256  _studentId, address _wallet) public {
         student_wallet[_studentId] = _wallet;
     }
+    function GetStudentWallet(int256 _studentId) public view returns (address)  {
+        return student_wallet[_studentId];
+    }
+    
+    function GetStudentId() public view returns (int256) {
+        return id;
+        
+    }
+    function SetStudentId(int256  _id) public returns (int256) {
+        id = _id;
+        return id;
+        
+    }
+    function SetStudentType(StudentType  _type) public  returns (StudentType) {
+        
+        student_type = _type ;
+        return student_type   ;
+    }
+    
 
     // Function to add funds to contract balance
-    function addFunds() public payable {
+    function AddFunds() public payable {
         balance += msg.value;
     }
+
+    
 }
 
 
